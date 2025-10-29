f5be1db7dac50686a4ced5985349d05cc8501aa6
angrrod <warrd2d2@gmail.com>
2025-10-29 15:05:00 +0100

    working playground code

--- Playground.py
+++ Playground.py
@@ -1,11 +1,3 @@
-#################################
-#####  codes to run in cli  #####
-#################################
-
-#####  run: conda activate gw  #####
-#####  conda install -c conda-forge lalsuite bilby gwpy matplotlib  #####
-
-###################################################################################################
 
 import gwpy  #visualization
 import bilby
@@ -15,10 +7,9 @@ import matplotlib.pyplot as plt
 from bilby.gw.source import lal_binary_black_hole
 
 #TODO: test for multiple detectors
-#TODO: ROQ is not good for E3
 
 wfv_args = dict(
-    waveform_approximant="IMRPhenomD", #IMRPhenomPv2 
+    waveform_approximant="IMRPhenomPv2", #IMRPhenomPv2 
     minimum_frequency=10.0,
     sampling_frequency=4096.0,
     reference_frequency=10.0,
@@ -48,7 +39,7 @@ ifo.set_strain_data_from_power_spectral_density(
 wg = bilby.gw.waveform_generator.WaveformGenerator(
     duration=wfv_args['duration'],
     sampling_frequency=wfv_args['sampling_frequency'],
-    frequency_domain_source_model=bilby.gw.source.binary_black_hole_roq,
+    frequency_domain_source_model=lal_binary_black_hole,
     waveform_arguments=wfv_args
 )
 
@@ -61,11 +52,11 @@ injct_params = dict(
     tilt_2=1.0,
     phi_12=1.7,  #part of the spin of the black hole
     phi_jl=0.3,
-    luminosity_distance=2000.0,
+    luminosity_distance=1000.0, #2000
     theta_jn=0.4, #angle of angular momentum
     psi=2.659,  #angle of polarization
     phase=1.3,
-    geocent_time=1126259642.413,
+    geocent_time=5,
     ra=1.375, #longituded
     dec=-1.2108,  #lattiude
 )
@@ -82,14 +73,16 @@ fs = wfv_args['sampling_frequency']
 #convert data
 ts = TimeSeries(td, sample_rate=fs, epoch=t0)
 
-#plot data
+#plot time domain of signal
 fig1 = ts.plot()
 ax = fig1.gca()
 ax.set_title("ET1 strain: time domain")
 ax.set_xlabel("Time [s]")
 ax.set_ylabel("Strain")
-plt.show()
+fig1.savefig("Plots/strain_time_domain.png", dpi=300, bbox_inches="tight")
+plt.close(fig1)
 
+#plot ASD
 asd = ts.asd(fftlength=4, method="median")  # choose fftlength to balance resolution vs variance
 fig2 = asd.plot()
 ax = fig2.gca()
@@ -98,4 +91,18 @@ ax.set_ylim(1e-25, 1e-20)  # tweak to taste
 ax.set_title("ET1 strain: amplitude spectral density")
 ax.set_xlabel("Frequency [Hz]")
 ax.set_ylabel("ASD [1/√Hz]")
-plt.show()
+plt.close(fig2)
+fig2.savefig("Plots/strain_asd.png", dpi=300, bbox_inches="tight")
+
+#freq domain
+H = ts.fft()                                # GWpy FrequencySeries (complex), ifft
+f = H.frequencies.value                      # Hz
+amp = np.abs(H.value) #absolute value of complex wave # |h(f)|
+
+fig = plt.figure()
+plt.loglog(f, amp)
+plt.xlabel("Frequency [Hz]")
+plt.ylabel(r"$|h(f)|$")
+plt.title("One-sided amplitude spectrum (RFFT)")
+fig.savefig("Plots/spectrum_rfft.png", dpi=300, bbox_inches="tight")
+plt.close(fig)
